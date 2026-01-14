08d1cc4d696710f537770c3bed8d00629d211fbb
Wilson Tovar <wtovaragudelo@gmail.com>
2026-01-14 21:24:40 +0100

    Refactor BullBoard admin route setup
    Extract BullBoard configuration and adapter initialization from the
    catch-all route to a separate module.

---
+++ server/routes/admin/queues.ts
@@ -0,0 +1,19 @@
+import { createBullBoard } from "@bull-board/api";
+import { BullMQAdapter } from "@bull-board/api/bullMQAdapter";
+import { H3Adapter } from "@bull-board/h3";
+import { queues } from "../../../utils/queue";
+import { H3Event } from "h3";
+
+const serverAdapter = new H3Adapter();
+serverAdapter.setBasePath("/admin/queues");
+
+createBullBoard({
+  queues: Object.values(queues).map((q) => new BullMQAdapter(q)),
+  serverAdapter,
+});
+
+const uiHandler = serverAdapter.registerHandlers();
+
+export const redirectToBullboard = async (event: H3Event) => {
+  return await uiHandler.handler(event);
+};
--- server/routes/admin/queues/[...].ts
+++ server/routes/admin/queues/[...].ts
@@ -1,14 +1,3 @@
-import { createBullBoard } from "@bull-board/api";
-import { BullMQAdapter } from "@bull-board/api/bullMQAdapter";
-import { H3Adapter } from "@bull-board/h3";
-import { queues } from "../../../utils/queue";
+import { redirectToBullboard } from "../queues";
 
-const serverAdapter = new H3Adapter();
-serverAdapter.setBasePath("/admin/queues");
-
-createBullBoard({
-  queues: Object.values(queues).map((q) => new BullMQAdapter(q)),
-  serverAdapter,
-});
-
-export default serverAdapter.registerHandlers();
+export default defineEventHandler(redirectToBullboard);
